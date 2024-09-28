347f99be0a1b15fcdd601634eda6b3b82c2e4860
NikhilUndare <nikundare111@gmail.com>
2024-09-28 23:05:23 +0530

    created the basic folder structure and added shadcn ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main >
+     <h1 className="text-6xl">Eventify</h1>
+      <Button >Hello</Button>
+    </main>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import {Poppins} from 'next/font/google'
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets : ['latin'],
+  weight : ['400','500','600','700'],
+  variable : '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: "Eventify",
+  description: "Eventify is a modern event organization web app",
+  
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>
+        {children}
+      </body>
+    </html>
+  );
+}
